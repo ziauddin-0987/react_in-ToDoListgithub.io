ed8cafd0a46894b0cc01ca29c1db64599bc17187
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-18 01:22:16 +0500

    this is code react i n to do list

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,44 @@ import { useState } from "react";
 import "./App.css";
 
 function ToDoList() {
-  
+  const [initial, setinitial] = useState();
+  const [getData, setgetData] = useState([]);
+  const getInput = (e) => {
+    console.log(e.target.value);
+    setinitial(e.target.value);
+  };
+  const btn = (d) => {
+    console.log(initial);
+    let dataStor = [...getData, initial];
+    setgetData(dataStor);
+  };
+
   return (
     <>
-    <div className="container">
-          <div className="header">
+      <div className="container">
+        <div className="header">
           <h1>To Do List</h1>
         </div>
-      <div className="toOneDiv">
-        <div className="toTowDiv">
-          <input
-            type="text"
-            className="inputs"
-            placeholder="print your idias "
-          />
+        <div className="toOneDiv">
+          <div className="toTowDiv">
+            <input
+              type="text"
+              className="inputs"
+              placeholder="print your idias "
+              onClick={getInput}
+            />
+          </div>
+          <div className="btn">
+            <button className="btn" onClick={btn}>
+              add
+            </button>
+          </div>
         </div>
-        <div className="btn">
-          <input type="button" value="add" onClick="clicks()" />
+        <div className="textParent" id="saveTextThisBob">
+          <div className="printUlText">
+            <li>{getData}</li>
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
